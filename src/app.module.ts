1a492360771cf4cb023ab5ff4f7b8c604fa65a20
GLFV <okolesnik753@gmail.com>
2023-01-12 15:35:15 -0500

    fix problem with db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,21 @@
+import { User } from './entities/user.entity';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
+    ConfigModule.forRoot({ envFilePath: '.env' }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
-      port: +process.env.DATABASE_PORT,
+      port: +process.env.DB_PORT,
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
-      entities: [],
+      entities: [User],
+      synchronize: true,
     }),
   ],
   controllers: [AppController],
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@nestjs/common';
-
 @Injectable()
 export class AppService {
   getHello(): string {
+    console.log(process.env.DB_DATABASE);
     return 'Hello World!';
   }
 }
